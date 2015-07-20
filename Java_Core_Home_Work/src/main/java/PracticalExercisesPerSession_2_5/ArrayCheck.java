4b87c04b8b81471436684ff59026ee9bde80580d
algumen <algumen@gmail.com>
2015-07-21 02:48:30 +0300

    Syncronization

---
+++ Java_Core_Home_Work/src/main/java/PracticalExercisesPerSession_2_5/ArrayCheck.java
@@ -0,0 +1,31 @@
+package PracticalExercisesPerSession_2_5;
+
+
+import java.util.Arrays;
+
+public class ArrayCheck {
+
+
+    public static void arrayCheck(int[] a, int b) {
+
+        if (a.length <= 3) {
+            System.out.println("Length must be greater than 3");
+            return;
+        }
+        if (!(b > 3 && b < 10)) {
+            System.out.println("Second parameter must be greater than 3 and less than 10");
+            return;
+        }
+        int[] c = new int[b];
+
+        for (int i = 2; i <= b; i++) {
+            c[i] = 10;
+        }
+        Arrays.sort(c);
+        for (int i = 0; i <= b; i++) {
+            System.out.println(c[i]);
+        }
+
+        return;
+    }
+}
---
+++ Java_Core_Home_Work/src/main/java/PracticalExercisesPerSession_2_5/MetodCheck.java
@@ -0,0 +1,13 @@
+package PracticalExercisesPerSession_2_5;
+
+public class MetodCheck {
+
+    public static void main(String[] args) {
+        int[] a = new int[]{4, 5, 6, 7};
+        int b =5;
+
+        arrayCheck(a,b);
+
+    }
+
+}
